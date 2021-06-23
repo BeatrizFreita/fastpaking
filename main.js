d3e56606579b54a17c90763fc6c682f77296c734
Beatriz Freitas <beatrizfreitasxbia@gmail.com>
2021-06-23 13:06:32 -0300

    Atualindo

---
+++ main.js
@@ -0,0 +1,4 @@
+'use strict';
+
+const openModalPrecos = () => document.querySelector('#modal-preco').classList.add('active');
+const closeModalPrecos = () => document.querySelector('#modal-preco').classList.remove('active');
